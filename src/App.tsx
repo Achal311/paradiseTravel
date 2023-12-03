a7a7070b2198f3dbf5b750f664c4d270f17281b6
Achal311 <achalmehta01@gmail.com>
2023-12-04 04:45:22 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,44 @@
+// import { useState } from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+function App() {
+  // const [count, setCount] = useState(0);
+  const imgURL = [
+    "/src/assets/bgimg.png",
+    "/src/assets/bgimg.png",
+    "/src/assets/bgimg.png",
+    "/src/assets/bgimg.png",
+    "/src/assets/bgimg.png",
+  ];
+  return (
+    <>
+      <Navbar></Navbar>
+      <div className="home">
+        <h1 className="heading-home">
+          travel <br /> as if you <br /> are in
+        </h1>
+        <div className="image-container">
+          <img
+            className="paradise-text"
+            src="/src/assets/whiteParadise.png"
+            alt=""
+          />
+          <img className="whiteRev" src="/src/assets/whiteRev.png" alt="" />
+        </div>
+      </div>
+      <div className="services"></div>
+      <div className="packages">
+        <div className="slider">
+          {imgURL.map((url, index) => (
+            <img src={url} alt="" key={index} className="slider-cards" />
+          ))}
+        </div>
+      </div>
+      <Footer></Footer>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,18 @@
+import "./Navbar.css";
+
+function Navbar() {
+  return (
+    <div className="navbar">
+      <div className="logo">
+        <img
+          className="logo"
+          src="/src/assets/logoAllwhite.png"
+          alt="company-logo"
+        />
+      </div>
+      <div className="navLinks"></div>
+    </div>
+  );
+}
+
+export default Navbar;
